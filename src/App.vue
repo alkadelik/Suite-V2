bfebe2b4a747778b2012f864b8f27381965c5846
Theo Iyonor <theo@leyyow.com>
2025-10-28 08:28:27 +0100

    feat: add consent banner for google analytics

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,11 @@
   <div>
     <router-view />
     <ToastContainer />
+    <ConsentBanner />
   </div>
 </template>
 
 <script setup lang="ts">
 import ToastContainer from "@components/core/ToastContainer.vue"
+import ConsentBanner from "@modules/shared/components/ConsentBanner.vue"
 </script>
