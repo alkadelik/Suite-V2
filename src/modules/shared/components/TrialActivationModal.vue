fb2531a8534967432afbfc4291a45543c5cf9173
Theo Iyonor <theo@leyyow.com>
2025-10-31 17:35:33 +0100

    feat: add mobile menu drawer and trail activation modal

---
+++ src/modules/shared/components/TrialActivationModal.vue
@@ -0,0 +1,49 @@
+<template>
+  <Modal
+    :open="open"
+    @close="emit('close')"
+    :show-header="false"
+    body-class="!rounded-2xl bg-white relative overflow-hidden"
+    max-width="2xl"
+  >
+    <div
+      class="pointer-events-none absolute inset-0 bg-[url('@/assets/images/celebration.png')] bg-cover opacity-30"
+    ></div>
+
+    <div class="relative flex justify-end">
+      <Icon name="close-circle" size="20" class="cursor-pointer" @click="emit('close')" />
+    </div>
+
+    <div
+      class="text-core-700 relative flex max-w-screen-md flex-col items-center gap-4 py-8 text-center"
+    >
+      <img src="@/assets/images/gift.png" class="h-20 w-auto object-contain md:h-[120px]" />
+
+      <h2 class="text-core-800 text-3xl font-bold md:text-4xl">A little something for you</h2>
+      <p class="text-lg md:text-xl">
+        We've gifted you 15 days of Leyyow Bloom - full access, totally free.
+      </p>
+      <p class="text-lg md:text-xl">
+        Start exploring premium features built to help your business shine.
+      </p>
+
+      <hr class="border-core-200 my-6 w-full" />
+
+      <p class="mb-6 text-xl">
+        Trial ends on:
+        <strong>{{ new Date().toLocaleString("en-US", { dateStyle: "long" }) }}</strong>
+      </p>
+
+      <AppButton label="Start Exploring Now" />
+    </div>
+  </Modal>
+</template>
+
+<script setup lang="ts">
+import AppButton from "@components/AppButton.vue"
+import Icon from "@components/Icon.vue"
+import Modal from "@components/Modal.vue"
+
+defineProps<{ open: boolean }>()
+const emit = defineEmits<{ close: [] }>()
+</script>
--- src/modules/shared/views/dashboard.vue
+++ src/modules/shared/views/dashboard.vue
@@ -1,17 +1,87 @@
 <template>
-  <div>
-    <SectionHeader size="lg" :title="`Welcome, ${getFullName(user as TNameObj)}`" />
+  <div class="px-2 py-4 md:p-4">
+    <SectionHeader size="lg" :title="`Good Afternoon, ${user?.first_name} 👋`" />
+
+    <div class="my-4 rounded-xl bg-white px-0 pb-2 md:my-8 md:p-6">
+      <h2 class="mb-3 text-lg font-semibold md:text-xl">Quick Actions</h2>
+      <div class="grid grid-cols-3 gap-2 md:grid-cols-5 md:gap-4">
+        <div
+          v-for="action in quickActions.slice(0, isMobile ? 3 : 5)"
+          :key="action.label"
+          class="cursor-pointer rounded-lg px-2 py-3 text-center md:p-4 md:text-left"
+          :class="action.color"
+        >
+          <div class="mb-1 flex items-center justify-between md:mb-4">
+            <div
+              class="border-core-200 mx-auto flex size-10 items-center justify-center rounded-xl border bg-white p-2 md:mx-0"
+            >
+              <Icon :name="action.icon" size="28" />
+            </div>
+
+            <Icon v-if="!isMobile" name="arrow-right" />
+          </div>
+          <span class="text-xs font-medium md:text-base">{{ action.label }}</span>
+        </div>
+      </div>
+    </div>
+
+    <SectionHeader
+      size="md"
+      title="Your store at a glance"
+      subtitle="Here’s what’s happening in your store this week."
+      class="mb-4"
+    />
+
+    <EmptyState
+      title="You don’t have any sales data yet!"
+      description="Once you start adding products and recording orders, your performance will show up here."
+      class="!min-h-[50vh]"
+    />
+
+    <!--  -->
     <WelcomeToTeamModal v-model="openModal" />
   </div>
 </template>
 
 <script setup lang="ts">
-import { getFullName, TNameObj } from "@/utils/format-strings"
 import SectionHeader from "@components/SectionHeader.vue"
 import { useAuthStore } from "@modules/auth/store"
 import WelcomeToTeamModal from "../components/WelcomeToTeamModal.vue"
 import { ref } from "vue"
+import Icon from "@components/Icon.vue"
+import EmptyState from "@components/EmptyState.vue"
+import { useMediaQuery } from "@vueuse/core"
 
 const { user } = useAuthStore()
 const openModal = ref(false)
+
+const isMobile = useMediaQuery("(max-width: 768px)")
+
+const quickActions = [
+  {
+    label: "Add a product",
+    icon: "box",
+    color: "bg-blue-50 text-blue-700",
+  },
+  {
+    label: "Record a sale",
+    icon: "bag",
+    color: "bg-green-50 text-green-700",
+  },
+  {
+    label: "Create popup",
+    icon: "calendar-tick",
+    color: "bg-purple-50 text-purple-700",
+  },
+  {
+    label: "Add a customer",
+    icon: "profile-add",
+    color: "bg-primary-50 text-primary-700",
+  },
+  {
+    label: "Record expense",
+    icon: "receipt-add",
+    color: "bg-pink-50 text-pink-700",
+  },
+]
 </script>
